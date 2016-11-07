a86ee2922be6a8ce4dcd5f78332b87c3f8af4cf0
Baljeet Singh <baljeet@freshdesk.com>
2016-11-07 17:15:26 +0530

    Sample App from chapter 3 making html more dynamic and using rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'db05941e7a07e8ec847dc9e335e31f78e20424a07f3ab81ba62ab2eb48deacd65e2d28e8547d55f5bd894735c05c497a9659679f757936c581d7bb71f91cddf2'
